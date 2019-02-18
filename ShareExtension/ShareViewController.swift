ce08275830225541392f6b7d0a5c6688438ef1a7
Ameya Raje <ameyaraje@users.noreply.github.com>
2019-02-18 17:19:41 -0600

    added share extension

---
+++ ShareExtension/ShareViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ShareViewController.swift
+//  ShareExtension
+//
+//  Created by OSI on 2/18/19.
+//  Copyright © 2019 Ameya. All rights reserved.
+//
+
+import UIKit
+import Social
+import MobileCoreServices
+
+class ShareViewController: SLComposeServiceViewController {
+
+    override func isContentValid() -> Bool {
+        // Do validation of contentText and/or NSExtensionContext attachments here
+        return true
+    }
+
+    override func didSelectPost() {
+        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+    
+        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    }
+
+    override func configurationItems() -> [Any]! {
+        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+        return []
+    }
+    
+    override func viewDidLoad() {
+        
+        let extensionItem = extensionContext?.inputItems.first as! NSExtensionItem
+        let itemProvider = extensionItem.attachments?.first as! NSItemProvider
+        let propertyList = String(kUTTypePropertyList)
+        if itemProvider.hasItemConformingToTypeIdentifier(propertyList) {
+            itemProvider.loadItem(forTypeIdentifier: propertyList, options: nil, completionHandler: { (item, error) -> Void in
+                guard let dictionary = item as? NSDictionary else { return }
+                OperationQueue.main.addOperation {
+                    if let results = dictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary,
+                        let urlString = results["URL"] as? String,
+                        let url = NSURL(string: urlString) {
+                        print("URL retrieved: \(urlString)")
+                    }
+                }
+            })
+        } else {
+            print("error")
+        }
+    }
+
+}
